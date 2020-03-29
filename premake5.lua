bae63cc360f7b92671590663399f99f1b0aaf148
viral32111 <19510403+viral32111@users.noreply.github.com>
2020-03-29 13:13:40 +0100

    Updated premake5 script

--- premake5.lua
+++ premake5.lua
@@ -1,24 +1,57 @@
-workspace "gm_regex"
-	configurations {"Release"}
-	location ("project")
-
-project "gm_regex"
-	kind "SharedLib"
-	architecture "x86"
+solution "Regex"
 	language "C++"
-	includedirs "include"
-	targetdir "build"
-	symbols "On"
-
-	if (os.is("windows")) then targetsuffix "_win32" end
-	if (os.is("macosx")) then targetsuffix "_osx" end
-	if (os.is("linux")) then targetsuffix "_linux" end
-
-	configuration "Release"
-		optimize "Speed"
-		flags "StaticRuntime"
-
-	files {
-		"source/**.*",
-		"include/**.*",
+	location ("solutions/"..os.target().."-".._ACTION)
+	targetdir ( "build/"..os.target().."/")
+	includedirs {
+		"source/",
+		"include/"
 	}
+	if os.target() == "macosx" or os.target() == "linux" then
+		buildoptions{ "-std=c++11 -fPIC" }
+		linkoptions{ "-fPIC -static-libstdc++" }
+	end
+
+	configurations { "Release" }
+	platforms { "x86", "x86_64" }
+
+	if os.target() == "windows" then
+		defines{ "WIN32" }
+	elseif os.target() == "linux" then
+		defines{ "LINUX" }
+	end
+
+	local platform
+	if os.target() == "windows" then
+		platform = "win"
+	elseif os.target() == "macosx" then
+		platform = "osx"
+	elseif os.target() == "linux" then
+		platform = "linux"
+	else
+		error "Unsupported platform."
+	end
+
+	filter "platforms:x86"
+		architecture "x86"
+		if os.target() == "windows" then
+			targetname("gmsv_regex_".. platform.."32")
+		else
+			targetname("gmsv_regex_"..platform)
+		end
+	filter "platforms:x86_64"
+		architecture "x86_64"
+		targetname("gmsv_regex_"..platform.."64")
+
+	project "Regex"
+		symbols "On"
+		editandcontinue "Off"
+		vectorextensions "SSE"
+		floatingpoint "Fast"
+		optimize "On"
+
+		defines{ "GMMODULE", "NDEBUG" }
+		files{ "source/**.*", "include/**.*" }
+		kind "SharedLib"
+		targetprefix ("")
+		targetextension (".dll")
+		targetdir("build/"..os.target())
